64036faf7b3d04b7fcf12103711d68be1c459803
yuyutw123 <yuyutw123@gmail.com>
2022-10-08 04:11:34 +0800

    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,37 @@
 window.addEventListener('load', () => {
-    todos = JSON.parse(localStorage.getItem('todos')) || [];
-    const nameInput = document.querySelector('#name');
-    const newTodoForm = document.querySelector('#new-todo-form');
+	todos = JSON.parse(localStorage.getItem('todos')) || [];
+	const nameInput = document.querySelector('#name');
+	const newTodoForm = document.querySelector('#new-todo-form');
 
-    const username = localStorage.getItem('username') || '';
+	const username = localStorage.getItem('username') || '';
 
-    nameInput.value = username;
+	nameInput.value = username;
 
-    nameInput.addEventListener('change', (e) => {
-        localStorage.setItem('username', e.target.value);
-    })
+	nameInput.addEventListener('change', (e) => {
+		localStorage.setItem('username', e.target.value);
+	})
 
-    newTodoForm.addEventListener('submit', (e) => {
-        e.preventDefault();
+	newTodoForm.addEventListener('submit', e => {
+		e.preventDefault();
 
-        const todo = {
-            content: e.target.elements.content.value,
-            category: e.target.elements.category.value,
-            done: false,
-            createdAt: new Date().getTime()
-        }
+		const todo = {
+			content: e.target.elements.content.value,
+			category: e.target.elements.category.value,
+			done: false,
+			createdAt: new Date().getTime()
+		}
 
-        todos.push(todo);
+		todos.push(todo);
 
-        localStorage.setItem('todo', JSON.stringify(todos));
+		localStorage.setItem('todos', JSON.stringify(todos));
 
-        // Reset the form
-        e.target.reset();
+		// Reset the form
+		e.target.reset();
 
-        DisplayTodos()
-    })
-    
-    DisplayTodos()
+		DisplayTodos()
+	})
+
+	DisplayTodos()
 })
 
 function DisplayTodos () {
@@ -39,36 +39,35 @@ function DisplayTodos () {
 	todoList.innerHTML = "";
 
 	todos.forEach(todo => {
-        const todoItem = document.createElement('div');
-        todoItem.classList.add('todo-item');
+		const todoItem = document.createElement('div');
+		todoItem.classList.add('todo-item');
 
-        const label = document.createElement('label');
-        const input = document.createElement('input');
-        const span = document.createElement('span');
-        const content = document.createElement('div');
-        const actions = document.createElement('div');
+		const label = document.createElement('label');
+		const input = document.createElement('input');
+		const span = document.createElement('span');
+		const content = document.createElement('div');
+		const actions = document.createElement('div');
 		const edit = document.createElement('button');
-        const deleteButton = document.createElement('button');
+		const deleteButton = document.createElement('button');
 
-        input.type = 'checkbox';
-        input.checked = todo.done;
+		input.type = 'checkbox';
+		input.checked = todo.done;
+		span.classList.add('bubble');
+		if (todo.category == 'personal') {
+			span.classList.add('personal');
+		} else {
+			span.classList.add('business');
+		}
+		content.classList.add('todo-content');
+		actions.classList.add('actions');
+		edit.classList.add('edit');
+		deleteButton.classList.add('delete');
 
-        span.classList.add('bubble');
-        if(todo.category == 'personal') {
-            span.classList.add('personal');
-        } else {
-            span.classList.add('school');
-        }
-        content.classList.add('todo-content');
-        actions.classList.add('actions');
-        edit.classList.add('edit');
-        deleteButton.classList.add('delete');
+		content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+		edit.innerHTML = 'Edit';
+		deleteButton.innerHTML = 'Delete';
 
-        content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
-        edit.innerHTML = "編輯";
-        deleteButton.innerHTML = "刪除";
-
-        label.appendChild(input);
+		label.appendChild(input);
 		label.appendChild(span);
 		actions.appendChild(edit);
 		actions.appendChild(deleteButton);
@@ -78,40 +77,42 @@ function DisplayTodos () {
 
 		todoList.appendChild(todoItem);
 
-        if(todo.done) {
-            todoItem.classList.add("done");
-        }
+		if (todo.done) {
+			todoItem.classList.add('done');
+		}
+		
+		input.addEventListener('change', (e) => {
+			todo.done = e.target.checked;
+			localStorage.setItem('todos', JSON.stringify(todos));
 
-        input.addEventListener('change' , (e) => {
-            todo.done = e.target.checked;
-            localStorage.setItem('todo', JSON.stringify(todos));
+			if (todo.done) {
+				todoItem.classList.add('done');
+			} else {
+				todoItem.classList.remove('done');
+			}
 
-            if(todo.done) {
-                todoItem.classList.add("done");
-            } else {
-                todoItem.classList.remove("done");
-            }
+			DisplayTodos()
 
-            DisplayTodos();
-        })
+		})
 
-        edit.addEventListener('click', (e) => {
-            const input = content.querySelector('input');
-            input.removeAttribute('readonly');
-            input.focus();
-            input.addEventListener('blur', (e) => {
-                input.setAttribute('readonly', true);
-                todo.content = e.target.value;
-                localStorage.setItem('todos', JSON.stringify(todos));
-                DisplayTodos();
-            })
-        })
+		edit.addEventListener('click', (e) => {
+			const input = content.querySelector('input');
+			input.removeAttribute('readonly');
+			input.focus();
+			input.addEventListener('blur', (e) => {
+				input.setAttribute('readonly', true);
+				todo.content = e.target.value;
+				localStorage.setItem('todos', JSON.stringify(todos));
+				DisplayTodos()
 
-        deleteButton.addEventListener('click', (e) => {
+			})
+		})
+
+		deleteButton.addEventListener('click', (e) => {
 			todos = todos.filter(t => t != todo);
 			localStorage.setItem('todos', JSON.stringify(todos));
 			DisplayTodos()
 		})
 
-    });
+	})
 }
